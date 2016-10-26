22105cbb517dfb44f7fd358c751d7b5524703402
Alexandr Semenov <alexandrsemenov@MacBook-Air-Alexandr.local>
2016-10-26 14:12:22 +0300

    Initial commit

---
+++ src/FirstClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by alexandrsemenov on 26.10.16.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
---
+++ src/ThirdClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by alexandrsemenov on 26.10.16.
+ */
+public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
