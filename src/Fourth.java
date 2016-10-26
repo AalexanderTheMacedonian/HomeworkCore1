263eadc9a70c091a058dea2f718ccca9d5f659d6
AlexandrTheMacedonian <Sanyasemenov96@gmail.com>
2016-10-26 14:16:03 +0300

    Test commit

--- src/Fourth.java
+++ src/Fourth.java
@@ -3,6 +3,6 @@
  */
 public class Fourth {
     public static void main(String[] args) {
-        System.out.println("Мой первый класс на Java");
+        System.out.println("Мой первый класс на Java.");
     }
 }
--- src/ThirdClass.java
+++ src/ThirdClass.java
@@ -3,6 +3,6 @@
  */
 public class ThirdClass {
     public static void main(String[] args) {
-        System.out.println("Мой первый класс на Java");
+        System.out.println("Мой первый класс на Java.");
     }
 }
